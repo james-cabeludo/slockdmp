18ecb50b5bddce3741753827037c3c2aceacc820
Vinicius <viniciusxyz@protonmail.com>
2024-07-18 13:04:58 -0300

    slock with image

---
+++ config.h
@@ -0,0 +1,6 @@
+/* user and group to drop privileges to */
+static const char *user = "nobody";
+static const char *group = "nobody";
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 0;
